881d26301eec7398ce3c88ef23c8130efb6e1114
Stéphan Kochen <stephan@kochen.nl>
2020-04-24 14:07:59 +0200

    Update portier-broker to 0.3.1

--- server-config/portier/pkgs/portier-broker/default.nix
+++ server-config/portier/pkgs/portier-broker/default.nix
@@ -1,12 +1,12 @@
 { fetchurl, bash, glibc, coreutils, gnutar, gzip, patchelf, openssl }:
 
 derivation {
-  name = "portier-broker-0.3.0";
+  name = "portier-broker-0.3.1";
   system = "x86_64-linux";
 
   src = fetchurl {
-    url = "https://github.com/portier/portier-broker/releases/download/v0.3.0/Portier-Broker-v0.3.0-Linux-x86_64.tgz";
-    hash = "sha256-7ucklRaMzkjqCIGYrCjczyXDfOWHcI4QZ/PBdpIL6ZM=";
+    url = "https://github.com/portier/portier-broker/releases/download/v0.3.1/Portier-Broker-v0.3.1-Linux-x86_64.tgz";
+    hash = "sha256-w0ePvhgZPkMwfl6DuOEUsKxTor33rJIXQqelvQRrGZI=";
   };
 
   inherit glibc coreutils gnutar gzip patchelf;
