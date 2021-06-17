11614e1c4e44213b72a9a4dccf2430410d97a2df
Stéphan Kochen <git@stephank.nl>
2021-06-17 21:46:08 +0200

    Small tweaks to build

--- flake.nix
+++ flake.nix
@@ -2,11 +2,16 @@
 
   inputs.nixpkgs.url = "nixpkgs/nixos-21.05-small";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: rec {
+
     nixosConfigurations.public-portier = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./server-config/configuration.nix ];
     };
+
+    # For easy `nix build`.
+    defaultPackage.x86_64-linux = nixosConfigurations.public-portier.config.system.build.toplevel;
+
   };
 
 }
