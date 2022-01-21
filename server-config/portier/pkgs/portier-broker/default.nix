c318f2421832f0538149bd69f239e0d8423f2f32
Stéphan Kochen <git@stephank.nl>
2022-01-21 13:45:59 +0100

    Upgrade portier-broker to 0.6.0

--- server-config/portier/pkgs/portier-broker/default.nix
+++ server-config/portier/pkgs/portier-broker/default.nix
@@ -1,12 +1,12 @@
 { fetchurl, bash, glibc, coreutils, gnutar, gzip, patchelf, openssl }:
 
 derivation {
-  name = "portier-broker-0.5.2";
+  name = "portier-broker-0.6.0";
   system = "x86_64-linux";
 
   src = fetchurl {
-    url = "https://github.com/portier/portier-broker/releases/download/v0.5.2/Portier-Broker-v0.5.2-Linux-x86_64.tgz";
-    hash = "sha256-kSeo3zcECfZf8eYWgJ23SGgC4y0IU0qnZIa8P06tVKg=";
+    url = "https://github.com/portier/portier-broker/releases/download/v0.6.0/Portier-Broker-v0.6.0-Linux-x86_64.tgz";
+    hash = "sha256-XyQPba3UmDCKo1SrPLcei0KK1AhY61Kg5D2/9GlRXrI=";
   };
 
   inherit glibc coreutils gnutar gzip patchelf;
