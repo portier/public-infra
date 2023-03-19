fb7dae6eb48e948dcdfbfc6474074440758d3354
Stéphan Kochen <git@stephank.nl>
2023-03-19 20:10:29 +0100

    Remove patchelf, binaries now static (testing)

--- server-config/portier-testing/build-testing-broker.sh
+++ server-config/portier-testing/build-testing-broker.sh
@@ -6,8 +6,3 @@ mv Portier-Broker-* "$out"
 rm "$out/portier-broker"
 unzip "$testsrc" -d "$out"
 chmod a+x "$out/portier-broker"
-
-patchelf \
-  --set-interpreter "$glibc/lib/ld-linux-x86-64.so.2" \
-  --set-rpath "$openssl/lib" \
-  "$out/portier-broker"
