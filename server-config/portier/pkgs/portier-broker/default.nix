69760a7350d498590051939aa8c1ee1c8703eab7
Stéphan Kochen <git@stephank.nl>
2023-04-14 08:24:53 +0200

    Upgrade to portier-broker 0.8.2

--- server-config/portier/pkgs/portier-broker/default.nix
+++ server-config/portier/pkgs/portier-broker/default.nix
@@ -1,12 +1,12 @@
 { fetchurl, bash, coreutils, gnutar, gzip }:
 
 derivation {
-  name = "portier-broker-0.8.0";
+  name = "portier-broker-0.8.2";
   system = "x86_64-linux";
 
   src = fetchurl {
-    url = "https://github.com/portier/portier-broker/releases/download/v0.8.0/Portier-Broker-v0.8.0-Linux-x86_64.tgz";
-    sha256 = "0ri4rjxkskvp50ldfk5w1yj4f5c9l11jqyd2z9j3hicwi8f7smwr";
+    url = "https://github.com/portier/portier-broker/releases/download/v0.8.2/Portier-Broker-v0.8.2-Linux-x86_64.tgz";
+    sha256 = "07fq20r0vacfwmniab84rzbbx3l6yx0wg2a3m520psc9xnrila59";
   };
 
   inherit coreutils gnutar gzip;
