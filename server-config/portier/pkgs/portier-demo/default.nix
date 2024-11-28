4e4bcfbd2a692289308169ed0cdb85ec8e45ee1d
Stéphan Kochen <git@stephank.nl>
2024-11-28 20:57:19 +0100

    Update demo-rp

--- server-config/portier/pkgs/portier-demo/default.nix
+++ server-config/portier/pkgs/portier-demo/default.nix
@@ -10,8 +10,8 @@ in stdenv.mkDerivation {
   name = "portier-demo";
 
   src = fetchurl {
-    url = "https://github.com/portier/demo-rp/archive/42294b2084e1aaef643b62f3a6eb830965cb86d4.tar.gz";
-    hash = "sha256-mj6boWLuMhaErYdcXVWJt2H9xo7IKaNZiiP6DzAv2iA=";
+    url = "https://github.com/portier/demo-rp/archive/7f261937da921a4a7de820375c9c238e01ff86fe.tar.gz";
+    hash = "sha256-jK/sq6zlLP9acAy71/0PVQ4lquCU497UBwgFGL764pE=";
   };
 
   buildInputs = [ python ];
